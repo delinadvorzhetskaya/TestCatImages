7a671dd3e5aee33d48dbdc38c80904eff44aa462
Delina Dvorzhetskaya <delina.dvorzhetskaya@gmail.com>
2021-11-23 13:36:54 +0300

    Initial Commit

---
+++ TestCatImages/TestCatImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCatImagesApp.swift
+//  TestCatImages
+//
+//  Created by Дэлина Дворжецкая on 23.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestCatImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
